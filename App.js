2542b8391f8e54b79a157670c426f2da2bf0a7df
Dash <dhanushdpandian@gmail.com>
2024-03-14 01:12:01 +0530

    learnin

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image ,SafeAreaView ,TouchableHighlight } from 'react-native';
 
 export default function App() {
+  console.log("dash");
+  const pre=()=>console.log("clicked");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={pre}>DHANUSHH</Text>
+      <TouchableHighlight onPress={pre}>
+      <Image
+  style={styles.tinyLogo}
+  source={{ 
+    width: 200,
+    height: 200,
+    uri: "https://picsum.photos/id/237/200/300" }}
+/>
+    </TouchableHighlight>
+    </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
